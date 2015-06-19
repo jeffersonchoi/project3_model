4b758b6484c12426f2cb9399bbb89a90c3cd8ca0
Jefferson Choi <jeffersonchoi@gmail.com>
2015-06-18 17:51:06 -0700

    working relationship between route and bus

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Route.create([
+  {name: "Route 1"},
+  {name: "Route 2"},
+  {name: "Route 3"},
+  {name: "Route 4"}
+  ])
+
+Bus.create([
+  {name: "Bus 1"},
+  {name: "Bus 2"},
+  {name: "Bus 3"},
+  {name: "Bus 4"}
+  ])
+
+
+Bus.all.each do |bus|
+
+  Route.all.sample(2).each do |route|
+    route.buses << bus
+
+  end
+
+
+end
