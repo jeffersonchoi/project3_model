0b57bbb05dee7aeca16e7236d59c4ccd964b2ed7
Jefferson Choi <jeffersonchoi@gmail.com>
2015-06-18 18:30:57 -0700

    proven working bus, route, route_stop, stop, stop_time model relationship

--- app/models/bus.rb
+++ app/models/bus.rb
@@ -1,6 +1,6 @@
 class Bus < ActiveRecord::Base
   belongs_to :route
-  has_many :route_stops, through: :routes
-  has_many :stops, through: :stop_times
+  has_many :route_stops, through: :route
+  has_many :stop_times
   has_many :stops, through: :stop_times
 end
