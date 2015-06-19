a079c48f495e6674452dfa5042126cf16d5e1344
Jefferson Choi <jeffersonchoi@gmail.com>
2015-06-19 14:22:16 -0700

    can show the direction of the bus

--- app/controllers/buses_controller.rb
+++ app/controllers/buses_controller.rb
@@ -17,7 +17,32 @@ class BusesController < ApplicationController
       Bus.create(latitude: bus["latitude"], name: bus["id"],
           longitude: bus["longitude"])
 
+    end
   end
+
+
+  #function to get bus directoin
+  def show
+    @buses = Bus.getBuses["items"][0]["run_id"].split("_").last
+    if @buses == "0"
+      @direction = "EastBound"
+    elsif @buses == "1"
+      @direction = "WestBound"
+    else
+      @direction = "none"
+    end
+
+    respond_to do |format|
+      format.html {
+        render
+      }
+      format.json {
+        render json: @direction
+      }
+    end
+
+
   end
+
 
 end
--- app/models/bus.rb
+++ app/models/bus.rb
@@ -12,4 +12,6 @@ class Bus < ActiveRecord::Base
 
 
 
+
+
 end
