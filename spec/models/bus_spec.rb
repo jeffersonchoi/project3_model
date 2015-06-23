215f5487d721ec8c60530e3b7b0bf07843537092
Paul Aglione <aglionep@gmail.com>
2015-06-23 16:41:53 -0700

    test bus model lat and long

--- spec/models/bus_spec.rb
+++ spec/models/bus_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Bus, type: :model do
-  
-end
+  it "is invalid without a latitude" do
+  	bus = Bus.new(name: "Paul's Bus", latitude: nil, longitude: 45.23)
+  		expect(bus).to be_invalid
+  	end
+
+  	it "is invalid without a longitude" do
+  		bus = Bus.new(name: "Paul's Bus", latitude: 48.52, longitude: nil)
+  			expect(bus).to be_invalid
+  	end		
+end	
+
