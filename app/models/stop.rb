7d4021523b04a3fa423ea38cbbd442f0801070de
Jefferson Choi <jeffersonchoi@gmail.com>
2015-06-23 10:19:57 -0700

    make 6 tests for stop.rb

--- app/models/stop.rb
+++ app/models/stop.rb
@@ -4,6 +4,6 @@ class Stop < ActiveRecord::Base
   has_many :stop_times, through: :route_stops
   has_many :buses, through: :stop_times
 
-  validates :latitude, presence: true
-
+  validates :latitude, :longitude , presence: true, numericality: { only_float: true }
+  validates :api_id, presence: true, numericality: { only_integer: true }
 end
