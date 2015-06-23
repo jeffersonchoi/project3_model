fad379f20f8985e7fdd67abf3270f61983175c30
Andrew Ham <hampossible@gmail.com>
2015-06-23 10:23:53 -0700

    created model tests for routes

--- spec/models/route_spec.rb
+++ spec/models/route_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Route, type: :model do
-  
+  it "is invalid without an api_id" do
+      route = Route.new(name: "Andrew's Route", api_id: nil)
+      expect(route).to be_invalid
+  end
+
+   it "api_id is an integer" do
+      route = Route.new(name: "Andrew's Route", api_id: "Andrew")
+      expect(route).to be_invalid
+  end
 end
