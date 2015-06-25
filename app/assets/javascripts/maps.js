8c27ee35a809ed3492a314716393c7ae380bda2d
Andrew Ham <hampossible@gmail.com>
2015-06-24 23:39:03 -0700

    Edited javascript page to allow for differentiation of 3 different routes using route-embedded JSON

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -21,38 +21,68 @@ function initialize() {
 // Defines variable "bounds" as the LatLngBounds of our map
     var bounds = new google.maps.LatLngBounds();
 // For loop to capture the latitudes and longitudes of each of the items in our array
-    var stopsPath = [];
-
+    var stopsPath704 = [];
+    var stopsPath733 = [];
+    var stopsPath734 = [];
+// Looping through 3 different routes via json reference
     for (var i = 0; i < results.length; i++) {
-      stops = new google.maps.LatLng(results[i].latitude, results[i].longitude);
-      stopsPath.push(stops);
+      if (results[i].routes[0].api_id == 704) {
+        var stops704 = new google.maps.LatLng(results[i].latitude, results[i].longitude);
+        stopsPath704.push(stops704);
+      }
+      if (results[i].routes[0].api_id == 733) {
+        var stops733 = new google.maps.LatLng(results[i].latitude, results[i].longitude);
+        stopsPath733.push(stops733);
+      }
+      if (results[i].routes[0].api_id == 734) {
+        var stops734 = new google.maps.LatLng(results[i].latitude, results[i].longitude);
+        stopsPath734.push(stops734);
+      }
     };
-
-    var flightPath = new google.maps.Polyline({
-      path: stopsPath,
+// Coloring route lines of different routes 
+    var path704 = new google.maps.Polyline({
+      path: stopsPath704,
       geodesic: true,
       strokeColor: '#FF0000',
       strokeOpacity: 1.0,
       strokeWeight: 2
     });
 
+    var path733 = new google.maps.Polyline({
+      path: stopsPath733,
+      geodesic: true,
+      strokeColor: '#0000FF',
+      strokeOpacity: 1.0,
+      strokeWeight: 2
+    });
+
+    var path734 = new google.maps.Polyline({
+      path: stopsPath734,
+      geodesic: true,
+      strokeColor: '#22FF00',
+      strokeOpacity: 1.0,
+      strokeWeight: 2
+    });
+// Creating markers for all stop positions and 
     for (var i = 0; i < results.length; i++) {
-      var stopPositions = new google.maps.LatLng(results[i].latitude, results[i].longitude)
+      var allStopPositions = new google.maps.LatLng(results[i].latitude, results[i].longitude)
       var marker = new google.maps.Marker({
-        position: stopPositions,
+        position: allStopPositions,
         icon: {
           path: google.maps.SymbolPath.CIRCLE,
           scale: 3
         },
       });
-      var myLatlng = stopPositions;
       marker.setMap(my_map);
-      bounds.extend(myLatlng);
+      bounds.extend(allStopPositions);
     }
 
 // Fits bounds of the map according to LatLngbounds of our map as defined by coordinates of existing data
     my_map.fitBounds(bounds);
-    flightPath.setMap(my_map);
+// Setting map for 3 different route paths
+    path704.setMap(my_map);
+    path733.setMap(my_map);
+    path734.setMap(my_map);
   })
 
 }
