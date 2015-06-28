d5471c76f3fcd5e3bd33650202ec6c8cba3d533f
Andrew Ham <hampossible@gmail.com>
2015-06-28 14:09:28 -0700

    changed marge conflcits in schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20150627232046) do
-=======
 ActiveRecord::Schema.define(version: 20150628194612) do
->>>>>>> new_branch
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
