984669197977f294b2af9ac67e03893c6dbc9e5f
Jefferson Choi <jeffersonchoi@gmail.com>
2015-06-28 12:47:08 -0700

    add direction to stop table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150627035730) do
+ActiveRecord::Schema.define(version: 20150628194612) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,6 +70,7 @@ ActiveRecord::Schema.define(version: 20150627035730) do
     t.datetime "updated_at", null: false
     t.integer  "api_id"
     t.integer  "seconds"
+    t.string   "direction"
   end
 
   add_foreign_key "buses", "routes"
