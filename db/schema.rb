91ab923c5f51a6e7cf8310dee4087b316820e3c3
Andrew Ham <hampossible@gmail.com>
2015-06-28 14:38:26 -0700

    hardcoded stops into seeds.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150627232046) do
+ActiveRecord::Schema.define(version: 20150628194612) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,6 +70,7 @@ ActiveRecord::Schema.define(version: 20150627232046) do
     t.datetime "updated_at", null: false
     t.integer  "api_id"
     t.integer  "seconds"
+    t.string   "direction"
   end
 
   create_table "tables", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -52,7 +52,459 @@
 
       # Load the stops for the route
       i = 0
-      stops = HTTParty.get("http://api.metro.net/agencies/lametro/routes/" + r["id"] + "/sequence/")
+
+      Stop.destroy_all
+
+      stops = Stop.create([
+          {
+            id: 1,
+            name: "2nd / Santa Monica",
+            latitude: 34.0150299,
+            longitude: -118.49735,
+            api_id: 29
+          },
+          {
+            id: 2,
+            name: "Ocean / Santa Monica",
+            latitude: 34.0140399,
+            longitude: -118.4977299,
+            api_id: 4096
+          },
+          {
+            id: 3,
+            name: "Santa Monica / 4th",
+            latitude: 34.0166499,
+            longitude: -118.49468,
+            api_id: 14360
+          },
+          {
+            id: 4,
+            name: "Santa Monica / Bundy",
+            latitude: 34.0405699,
+            longitude: -118.46304,
+            api_id: 6041
+          },
+          {
+            id: 5,
+            name: "Santa Monica / Barrington",
+            latitude: 34.0437499,
+            longitude: -118.45578,
+            api_id: 6039
+          },
+          {
+            id: 6,
+            name: "Santa Monica / Sepulveda",
+            latitude: 34.0477999,
+            longitude: -118.44351,
+            api_id: 14424
+          },
+          {
+            id: 7,
+            name: "Santa Monica / Westwood",
+            latitude: 34.0498699,
+            longitude: -118.43622,
+            api_id: 6058
+          },
+          {
+            id: 8,
+            name: "Santa Monica / Beverly Glen",
+            latitude: 34.05769,
+            longitude: -118.4237199,
+            api_id: 6040
+          },
+          {
+            id: 9,
+            name: "Santa Monica / Avenue Of The Stars",
+            latitude: 34.0615399,
+            longitude: -118.41797,
+            api_id: 5917
+          },
+          {
+            id: 10,
+            name: "Santa Monica / Wilshire",
+            latitude: 34.06682,
+            longitude: -118.4104899,
+            api_id: 6050
+          },
+          {
+            id: 11,
+            name: "Santa Monica / Crescent",
+            latitude: 34.07316,
+            longitude: -118.4012699,
+            api_id: 5985
+          },
+          {
+            id: 12,
+            name: "Santa Monica / San Vicente",
+            latitude: 34.0849099,
+            longitude: -118.38363,
+            api_id: 14378
+          },
+          {
+            id: 13,
+            name: "Santa Monica / La Cienega",
+            latitude: 34.0893299,
+            longitude: -118.37608,
+            api_id: 7946
+          },
+          {
+            id: 14,
+            name: "Santa Monica / Sweetzer",
+            latitude: 34.09059,
+            longitude: -118.37044,
+            api_id: 6031
+          },
+          {
+            id: 15,
+            name: "Santa Monica / Fairfax",
+            latitude: 34.0906999,
+            longitude: -118.36172,
+            api_id: 6014
+          },
+          {
+            id: 16,
+            name: "Santa Monica / Vista",
+            latitude: 34.0906399,
+            longitude: -118.3521399,
+            api_id: 6052
+          },
+          {
+            id: 17,
+            name: "Santa Monica / La Brea",
+            latitude: 34.09059,
+            longitude: -118.3443,
+            api_id: 6023
+          },
+          {
+            id: 18,
+            name: "Santa Monica / Highland",
+            latitude: 34.09057,
+            longitude: -118.33819,
+            api_id: 6019
+          },
+          {
+            id: 19,
+            name: "Santa Monica / Vine",
+            latitude: 34.0905899,
+            longitude: -118.32633,
+            api_id: 5386
+          },
+          {
+            id: 20,
+            name: "Santa Monica / Western",
+            latitude: 34.0906599,
+            longitude: -118.30891,
+            api_id: 14436
+          },
+          {
+            id: 21,
+            name: "Santa Monica / Normandie",
+            latitude: 34.0907,
+            longitude: -118.2997799,
+            api_id: 5387
+          },
+          {
+            id: 22,
+            name: "Santa Monica / Vermont",
+            latitude: 34.0907699,
+            longitude: -118.29203,
+            api_id: 6033
+          },
+          {
+            id: 23,
+            name: "Santa Monica / Sanborn",
+            latitude: 34.0924399,
+            longitude: -118.28078,
+            api_id: 1443
+          },
+          {
+            id: 24,
+            name: "Sunset / Parkman",
+            latitude: 34.08296,
+            longitude: -118.27353,
+            api_id: 17306
+          },
+          {
+            id: 25,
+            name: "Sunset / Alvarado",
+            latitude: 34.0775399,
+            longitude: -118.26322,
+            api_id: 5400
+          },
+          {
+            id: 26,
+            name: "Sunset / Echo Park",
+            latitude: 34.0767099,
+            longitude: -118.25755,
+            api_id: 8011
+          },
+          {
+            id: 27,
+            name: "Sunset / Elysian Pk Dodger Stadium",
+            latitude: 34.0726099,
+            longitude: -118.2512499,
+            api_id: 2421
+          },
+          {
+            id: 28,
+            name: "Sunset / Figueroa",
+            latitude: 34.0626699,
+            longitude: -118.24651,
+            api_id: 8040
+          },
+          {
+            id: 29,
+            name: "Cesar E Chavez / Grand",
+            latitude: 34.0603199,
+            longitude: -118.2436999,
+            api_id: 668
+          },
+          {
+            id: 30,
+            name: "Cesar E Chavez / Spring",
+            latitude: 34.0581,
+            longitude: -118.2388099,
+            api_id: 660
+          },
+          {
+            id: 31,
+            name: "Patsaouras Transit Plaza",
+            latitude: 34.05539,
+            longitude: -118.2331199,
+            api_id: 30000
+          },
+          {
+            id: 32,
+            name: "Center / Jackson Terminal",
+            latitude: 34.0512399,
+            longitude: -118.23093,
+            api_id: 1605
+          },
+          {
+            id: 33,
+            name: "Patsaouras Transit Plaza",
+            latitude: 34.05539,
+            longitude: -118.2331199,
+            api_id: 30000
+          },
+          {
+            id: 34,
+            name: "Cesar E Chavez / Broadway",
+            latitude: 34.05881,
+            longitude: -118.2404,
+            api_id: 9221
+          },
+          {
+            id: 35,
+            name: "Cesar E Chavez / Grand",
+            latitude: 34.0609,
+            longitude: -118.24398,
+            api_id: 16927
+          },
+          {
+            id: 36,
+            name: "Sunset / Figueroa",
+            latitude: 34.0629399,
+            longitude: -118.24641,
+            api_id: 16329
+          },
+          {
+            id: 37,
+            name: "Sunset / Elysian Pk Dodger Stadium",
+            latitude: 34.0726199,
+            longitude: -118.2509199,
+            api_id: 2422
+          },
+          {
+            id: 38,
+            name: "Sunset / Echo Park",
+            latitude: 34.0770699,
+            longitude: -118.25771,
+            api_id: 16304
+          },
+          {
+            id: 39,
+            name: "Sunset / Alvarado",
+            latitude: 34.0778,
+            longitude: -118.2631499,
+            api_id: 16318
+          },
+          {
+            id: 40,
+            name: "Sunset / Silver Lake",
+            latitude: 34.0831499,
+            longitude: -118.2732799,
+            api_id: 8591
+          },
+          {
+            id: 41,
+            name: "Sunset / Hyperion",
+            latitude: 34.0920999,
+            longitude: -118.2798399,
+            api_id: 16309
+          },
+          {
+            id: 42,
+            name: "Santa Monica / Vermont",
+            latitude: 34.0909999,
+            longitude: -118.29194,
+            api_id: 14349
+          },
+          {
+            id: 43,
+            name: "Santa Monica / Normandie",
+            latitude: 34.0909599,
+            longitude: -118.3010899,
+            api_id: 8598
+          },
+          {
+            id: 44,
+            name: "Santa Monica / Western",
+            latitude: 34.0909099,
+            longitude: -118.30949,
+            api_id: 6056
+          },
+          {
+            id: 45,
+            name: "Santa Monica / Vine",
+            latitude: 34.0908699,
+            longitude: -118.32695,
+            api_id: 14432
+          },
+          {
+            id: 46,
+            name: "Santa Monica / Highland",
+            latitude: 34.09085,
+            longitude: -118.3389199,
+            api_id: 6018
+          },
+          {
+            id: 47,
+            name: "Santa Monica / La Brea",
+            latitude: 34.0908499,
+            longitude: -118.34457,
+            api_id: 14401
+          },
+          {
+            id: 48,
+            name: "Santa Monica / Gardner",
+            latitude: 34.09091,
+            longitude: -118.35357,
+            api_id: 14393
+          },
+          {
+            id: 49,
+            name: "Santa Monica / Fairfax",
+            latitude: 34.0909599,
+            longitude: -118.36122,
+            api_id: 14391
+          },
+          {
+            id: 50,
+            name: "Santa Monica / Sweetzer",
+            latitude: 34.0908399,
+            longitude: -118.3706199,
+            api_id: 14411
+          },
+          {
+            id: 51,
+            name: "Santa Monica / La Cienega",
+            latitude: 34.0898799,
+            longitude: -118.37604,
+            api_id: 16240
+          },
+          {
+            id: 52,
+            name: "Santa Monica / San Vicente",
+            latitude: 34.0849499,
+            longitude: -118.3844,
+            api_id: 6045
+          },
+          {
+            id: 53,
+            name: "Santa Monica / Canon",
+            latitude: 34.0726199,
+            longitude: -118.40253,
+            api_id: 1296
+          },
+          {
+            id: 54,
+            name: "Santa Monica / Wilshire",
+            latitude: 34.0673699,
+            longitude: -118.41018,
+            api_id: 2152
+          },
+          {
+            id: 55,
+            name: "Santa Monica / Avenue Of The Stars",
+            latitude: 34.0612099,
+            longitude: -118.4192,
+            api_id: 14363
+          },
+          {
+            id: 56,
+            name: "Santa Monica / Westwood",
+            latitude: 34.0498699,
+            longitude: -118.43783,
+            api_id: 14438
+          },
+          {
+            id: 57,
+            name: "Santa Monica / Sepulveda",
+            latitude: 34.0476099,
+            longitude: -118.44489,
+            api_id: 14431
+          },
+          {
+            id: 58,
+            name: "Santa Monica / Barrington",
+            latitude: 34.0437099,
+            longitude: -118.45653,
+            api_id: 14421
+          },
+          {
+            id: 59,
+            name: "Santa Monica / Bundy",
+            latitude: 34.04084,
+            longitude: -118.4630899,
+            api_id: 14423
+          },
+          {
+            id: 60,
+            name: "Santa Monica / 26th",
+            latitude: 34.03351,
+            longitude: -118.4742999,
+            api_id: 14359
+          },
+          {
+            id: 61,
+            name: "Santa Monica / 20th",
+            latitude: 34.02859,
+            longitude: -118.4803599,
+            api_id: 14357
+          },
+          {
+            id: 62,
+            name: "Santa Monica / Lincoln",
+            latitude: 34.0197199,
+            longitude: -118.49134,
+            api_id: 14379
+          },
+          {
+            id: 63,
+            name: "Broadway / 4th",
+            latitude: 34.0149399,
+            longitude: -118.4939,
+            api_id: 5969
+          },
+          {
+            id: 64,
+            name: "Ocean / Santa Monica",
+            latitude: 34.0140399,
+            longitude: -118.4977299,
+            api_id: 4096
+          },
+        ])
       stop29 = HTTParty.get("http://api.metro.net/agencies/lametro/stops/29/predictions/")
       stop4096 = HTTParty.get("http://api.metro.net/agencies/lametro/stops/4096/predictions/")
       stop14360 = HTTParty.get("http://api.metro.net/agencies/lametro/stops/14360/predictions/")
