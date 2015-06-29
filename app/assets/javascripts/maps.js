509db0b83820e9611241123eea51b6976aa4e832
Andrew Ham <hampossible@gmail.com>
2015-06-29 04:43:02 -0700

    added stop names and direction where necessary to infowindows

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -4,6 +4,10 @@ google.maps.event.addDomListener(window, 'page:load', initialize);
 
 var stopsPath704 = [];
 var busLocations = [];
+var testStops = [
+  new google.maps.LatLng(34.07316, -118.4012699),
+  new google.maps.LatLng(34.0849099, -118.38363)
+];
 // Initialize Google Map
 function initialize() {
 
@@ -39,20 +43,35 @@ function initialize() {
         },
       });
 
+      // Coloring route lines of different routes
+      var path704 = new google.maps.Polyline({
+        path: stopsPath704,
+        geodesic: true,
+        strokeColor: '#FF0000',
+        strokeOpacity: 1.0,
+        strokeWeight: 5
+      });
 
       var infowindow = new google.maps.InfoWindow();
 
       google.maps.event.addListener(marker, 'mouseover', function(marker, i) {
           return function (){
-            var content = '<div>'+results[0].stops[i].seconds+'</div>'
+            var stopName = results[0].stops[i].name
+            var secondsTillArrival = results[0].stops[i].seconds
+            var direction = results[0].stops[i].direction
+            if (results[0].stops[i].direction == null){
+              var content = "<p><b>Stop Name:</b></p>" + '<p>'+stopName+'</p>' + "<p><b>Time till arrival(seconds):</b></p>" + '<p>'+secondsTillArrival+'</p>'
+            } else {
+              var content = "<p><b>Stop Name:</b></p>" + '<p>'+stopName+'</p>' + "<p><b>Time till arrival(seconds):</b></p>" + '<p>'+secondsTillArrival+'</p>' + "<p><b>Direction (next bus):</b></p>" + '<p>'+direction+'</p>'
+            }
             infowindow.setContent(content);
             infowindow.open(my_map, marker);
           };
       }(marker,i));
 
-      google.maps.event.addListener(marker, 'click', function(marker, i) {
-          infowindow.close(my_map, marker);
-      }(marker, i));
+      // google.maps.event.addListener(marker, 'mouseout', function(marker, i) {
+      //     infowindow.close(my_map, marker);
+      // }(marker, i));
 
       marker.setMap(my_map);
 
@@ -63,21 +82,10 @@ function initialize() {
     for (var i = 0; i < results[0].buses.length; i++) {
       var bus_position = new google.maps.LatLng(results[0].buses[i].latitude, results[0].buses[i].longitude)
       busLocations.push(bus_position)
-      var marker = new google.maps.Marker({
-        position: bus_position,
-        icon: '/assets/bus_icon.png'
-      });
+  
+      }
       marker.setMap(my_map);
     }
-
-// Coloring route lines of different routes
-    var path704 = new google.maps.Polyline({
-      path: stopsPath704,
-      geodesic: true,
-      strokeColor: '#FF0000',
-      strokeOpacity: 1.0,
-      strokeWeight: 5
-    });
 
 // Fits bounds of the map according to LatLngbounds of our map as defined by coordinates of existing data
     my_map.fitBounds(bounds);
